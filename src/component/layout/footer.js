08c1274f23d943f25ebecb7029cfb649733fe635
OckJuYong <20201776@edu.hanbat.ac.kr>
2023-08-24 16:41:27 +0900

    secon

--- src/component/layout/footer.js
+++ src/component/layout/footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import "./footer.css";
 
 const Footer = () => {
-    return (
-        <div className="footer_page_container">여긴 Footer 부분이다.</div>
-    )
-}
 
-export default Footer
+  return (
+    <div className='footer_page_container'>
+
+    </div>
+  );
+};
+
+export default Footer;
